aef13869c9d95ec763911adbfa6acf8756628b02
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-05-01 14:52:20 +0900

    passed eslint

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,39 +1,38 @@
 const luxon = require('luxon')
+const today = luxon.DateTime.now()
+const argv = require('minimist')(process.argv.slice(2))
 
 const firstDay = luxon.DateTime.fromObject({
-  year: 2022,
-  month: 5,
+  year: argv.y = (typeof argv.y !== 'undefined') ? argv.y : today.year,
+  month: argv.m = (typeof argv.m !== 'undefined') ? argv.m : today.month,
   day: 1
 })
 
 const lastDay = firstDay.endOf('month')
 
-printHeader()
+function main () {
+  printHeader()
 
-for (let date = firstDay.day; date <= lastDay.day; date++) {
-  const currentDate = firstDay.plus({ day: date - 1 })
+  for (let date = firstDay.day; date <= lastDay.day; date++) {
+    const currentDate = firstDay.plus({ day: date - 1 })
 
-  formatDate(date)
-  if (currentDate.weekdayShort === 'Sat') {
-    console.log(String(date))
-  } else {
-    process.stdout.write(String(date))
+    if (currentDate.weekdayShort === 'Sat') {
+      console.log(String(date).padStart(3, ' '))
+    } else {
+      process.stdout.write(String(date).padStart(3, ' '))
+    }
+  }
+  if (lastDay.weekdayShort !== 'Sat') {
+    process.stdout.write('\n')
   }
 }
-console.log('\n')
 
 function printHeader () {
-  console.log('      ' + firstDay.year + '年' + firstDay.month + '月')
+  console.log('      ' + firstDay.year + '年 ' + firstDay.month + '月')
   console.log(' 日 月 火 水 木 金 土')
   if (firstDay.weekdayShort !== 'Sun') {
     process.stdout.write(String(' ').repeat(firstDay.weekday * 3))
   }
 }
 
-function formatDate (date) {
-  if (date < 10) {
-    process.stdout.write(String('  '))
-  } else {
-    process.stdout.write(String(' '))
-  }
-}
+main()
