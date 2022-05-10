b27ad4275b7dd0c9c385f7824e2cec0a41cdc4ea
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-05-10 11:13:59 +0900

    frame was completed

---
+++ 04.class/lib/memo.js
@@ -0,0 +1,29 @@
+﻿class DBAccessor {
+  static load() {
+    console.log('データベースにアクセスするクラスです')
+  }
+}
+
+class Memos {
+  index () {
+    console.log('一覧を表示します')
+  }
+
+  show () {
+    console.log('詳細を表示します')
+  }
+
+  create () {
+    console.log('新規作成します')
+  }
+
+  destroy () {
+    console.log('削除します')
+  }
+}
+
+DBAccessor.load()
+const memos = new Memos()
+memos.index()
+memos.create()
+memos.destroy()
