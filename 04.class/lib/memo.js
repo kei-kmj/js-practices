46aef1bd5d060602d41c43854e42993d5feaa30f
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-05-10 11:14:56 +0900

    implemented option receiving part

--- 04.class/lib/memo.js
+++ 04.class/lib/memo.js
@@ -5,25 +5,41 @@
 }
 
 class Memos {
-  index () {
+  index() {
     console.log('一覧を表示します')
   }
 
-  show () {
+  show() {
     console.log('詳細を表示します')
   }
 
-  create () {
+  create() {
     console.log('新規作成します')
   }
 
-  destroy () {
+  destroy() {
     console.log('削除します')
   }
 }
 
 DBAccessor.load()
 const memos = new Memos()
-memos.index()
-memos.create()
-memos.destroy()
+
+const command = require("commander")
+command
+    .option('-l, --index')
+    .option('-r, --read')
+    .option('-d, --destroy')
+
+command.parse(process.argv);
+
+const options = command.opts();
+if (options.index) {
+  memos.index()
+} else if (options.read) {
+  memos.show()
+} else if (options.destroy) {
+  memos.destroy()
+} else if (options.args === undefined) {
+  memos.create()
+}
