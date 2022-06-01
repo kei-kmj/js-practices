ef98656c71b189274597e00bfb461e7cc1b6454e
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-06-01 22:20:40 +0900

    implemented the behavior when there is no record in the table

--- 04.class/lib/memo.js
+++ 04.class/lib/memo.js
@@ -1,15 +1,32 @@
 const command = require('commander')
 const Enquirer = require('enquirer')
-const util = require('util')
 const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('memo.sqlite')
 
 class Memos {
-  static #dbAccessor() {
-    return new sqlite3.Database('memo.sqlite')
+
+  operate() {
+    db.run(`CREATE TABLE IF NOT EXISTS memos
+            (
+                id      INTEGER PRIMARY KEY,
+                content TEXT NOT NULL
+            )`, () => {
+      db.get('SELECT COUNT (*) FROM memos', (err, count) => {
+        if (err) {
+          console.log(err)
+        }
+        if (count['COUNT (*)'] === 0) {
+          console.log('メモはまだありません')
+          memos.create()
+        } else {
+          switchOperation()
+        }
+      })
+    })
   }
 
   list() {
-    Memos.#dbAccessor().all('SELECT id, content FROM memos', (err, rows) => {
+    db.all('SELECT id, content FROM memos', (err, rows) => {
       if (err) {
         console.log(err)
         return
@@ -21,29 +38,24 @@ class Memos {
   }
 
   show() {
-
-    const db = Memos.#dbAccessor()
     memos = []
-    const selectValue = function () {
-      return new Promise((resolve, reject) => {
-        db.all('SELECT * FROM memos', (err, rows) => {
-          if (err) return reject(err)
-          resolve(rows)
-        })
-      })
-    }
-    selectValue().then(rows => {
+    db.all('SELECT * FROM memos', async (err, rows) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+
       rows.forEach(row => {
         memos.push(`${row.id}:${row.content.split('\n')[0]}`)
       })
-      return memos
-    }).then(async (data) => {
+
       const questionShow = {
         type: 'select',
         name: 'show',
         message: '確認するメモを選んでください',
-        choices: data.concat('確認をやめる')
+        choices: memos.concat('確認をやめる')
       }
+
       const answer = await Enquirer.prompt(questionShow)
       if (answer.show === '確認をやめる') {
         console.log('処理を中止しました')
@@ -58,27 +70,86 @@ class Memos {
           })
         })
       }
+
+      //-->
     })
+    //
+    // new Promise((resolve) => {
+    //   db.all('SELECT * FROM memos', async (err, rows) => {
+    //     if (err) {
+    //       console.log(err)
+    //       return
+    //     }
+    //     resolve(rows)
+    //
+    //     rows.forEach(row => {
+    //       selectionItems.push(`${row.id}:${row.content.split('\n')[0]}`)
+    //     })
+    //
+    //     const questionShow = {
+    //       type: 'select',
+    //       name: 'show',
+    //       message: '確認するメモを選んでください',
+    //       choices: data.concat('確認をやめる')
+    //     }
+    //
+    //     const answer = await Enquirer.prompt(questionShow)
+    //     if (answer.show === '確認をやめる') {
+    //       console.log('処理を中止しました')
+    //     } else {
+    //       db.all('SELECT id, content FROM memos WHERE id = ?', answer.show.split(':')[0], (err, rows) => {
+    //         if (err) {
+    //           console.log(err)
+    //           return
+    //         }
+    //         rows.forEach((row) => {
+    //           console.log(row.content)
+    //         })
+    //       })
+    //     }
+    //
+    //     //-->
+    //   })
+    // }).then(rows => {
+    //   rows.forEach(row => {
+    //     selectionItems.push(`${row.id}:${row.content.split('\n')[0]}`)
+    //   })
+    //   return selectionItems
+    // }).then(async (data) => {
+    //   const questionShow = {
+    //     type: 'select',
+    //     name: 'show',
+    //     message: '確認するメモを選んでください',
+    //     choices: data.concat('確認をやめる')
+    //   }
+    //   const answer = await Enquirer.prompt(questionShow)
+    //   if (answer.show === '確認をやめる') {
+    //     console.log('処理を中止しました')
+    //   } else {
+    //     db.all('SELECT id, content FROM memos WHERE id = ?', answer.show.split(':')[0], (err, rows) => {
+    //       if (err) {
+    //         console.log(err)
+    //         return
+    //       }
+    //       rows.forEach((row) => {
+    //         console.log(row.content)
+    //       })
+    //     })
+    //   }
+    // })
   }
 
   create() {
-    Memos.#dbAccessor().run(`CREATE TABLE IF NOT EXISTS memos
-                             (
-                                 id      INTEGER PRIMARY KEY,
-                                 content TEXT NOT NULL
-                             )`)
-
     process.stdin.resume()
     process.stdin.setEncoding('utf8')
     let newMemo = ''
-    console.log(`新しいメモを作成します\n (Enter入力後にControl+Dで登録、中止する場合はControl+C)`)
+    console.log('新しいメモを作成します\n (Enter入力後にControl+Dで登録、中止する場合はControl+C)')
     process.stdin.on('data', function (chunk) {
       newMemo += chunk
     })
-
     process.stdin.on('end', async function () {
-      const statement = Memos.#dbAccessor().prepare('INSERT INTO memos (content) VALUES(?)')
-      await util.promisify(statement.run.bind(statement))(newMemo)
+      const statement = db.prepare('INSERT INTO memos (content) VALUES(?)')
+      statement.run.bind(statement)(newMemo)
       if (newMemo !== '') {
         console.log('メモを登録しました')
       }
@@ -86,17 +157,16 @@ class Memos {
   }
 
   destroy() {
-    const db = Memos.#dbAccessor()
     memos = []
-    const selectValue = function () {
-      return new Promise((resolve, reject) => {
-        db.all('SELECT * FROM memos', (err, rows) => {
-          if (err) return reject(err)
-          resolve(rows)
-        })
+    new Promise((resolve) => {
+      db.all('SELECT * FROM memos', (err, rows) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        resolve(rows)
       })
-    }
-    selectValue().then(rows => {
+    }).then(rows => {
       rows.forEach(row => {
         memos.push(row.id + ':' + row.content.split('\n')[0])
       })
@@ -121,21 +191,25 @@ class Memos {
 }
 
 let memos = new Memos()
-command
-    .option('-l, --list')
-    .option('-r, --read')
-    .option('-d, --destroy')
+memos.operate()
 
-command.parse(process.argv)
+function switchOperation() {
+  command
+      .option('-l, --list')
+      .option('-r, --read')
+      .option('-d, --destroy')
 
-const options = command.opts()
+  command.parse(process.argv)
 
-if (options.list) {
-  memos.list()
-} else if (options.read) {
-  memos.show()
-} else if (options.destroy) {
-  memos.destroy()
-} else {
-  memos.create()
+  const options = command.opts()
+
+  if (options.list) {
+    memos.list()
+  } else if (options.read) {
+    memos.show()
+  } else if (options.destroy) {
+    memos.destroy()
+  } else {
+    memos.create()
+  }
 }
